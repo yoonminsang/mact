cdfc7545ad7e594decf7a78af6874b13ac21b4bd
yoonminsang <apdlyooapdl@naver.com>
2022-08-26 00:01:42 +0900

    chore: example 파일 제거

--- src/lib/mact/example/index.ts
+++
@@ -1,8 +0,0 @@
-import { render } from '@/lib/mact';
-
-import App from './app';
-
-import './styles/reset.css';
-import './styles/index.css';
-
-render(new App({}), document.getElementById('root') as HTMLElement);
